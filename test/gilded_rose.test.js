f6730bcd503e09dd91b21749fe3d5992a3fc48d8
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-06-22 18:12:29 +0100

    test to check that item in the shop exists + test checking that quality decreasing by one

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -29,21 +29,47 @@ describe('Gilded Rose', () => {
 
   describe('class Shop', () => {
     it('has items instance property after initializing the class', () => {
-      const shop = new Shop([new Item('banana', 5, 5)]);
+      const item = { name: 'banana', sellIn: 5, quality: 5 };
+      const shop = new Shop([item]);
       expect(shop.items).toEqual([new Item('banana', 5, 5)]);
     });
 
-    describe('updateQuality function', () => {
+    describe('updateQuality with quality limitation', () => {
       it('should not have negative quality', () => {
-        const shop = new Shop([new Item('banana', 5, 5)]);
+        const item = { name: 'banana', sellIn: 5, quality: 5 };
+        const shop = new Shop([item]);
         shop.updateQuality();
         expect(shop.items[0].quality).toBeGreaterThanOrEqual(0);
       });
 
       it('should not have quality greater than 50', () => {
-        const shop = new Shop([new Item('banana', 5, 50)]);
+        const item = { name: 'banana', sellIn: 5, quality: 5 };
+        const shop = new Shop([item]);
         shop.updateQuality();
         expect(shop.items[0].quality).toBeLessThanOrEqual(50);
+      });
+    });
+
+    describe('updateQuality with standard items', () => {
+      let item;
+      let shop;
+
+      beforeEach(() => {
+        item = { name: 'banana', sellIn: 5, quality: 5 };
+        shop = new Shop([item]);
+      });
+
+      it('should have an item in the shop', () => {
+        expect(shop.items[0]).toEqual({
+          name: 'banana',
+          sellIn: 5,
+          quality: 5,
+        });
+      });
+
+      it('should update quality decreasing by one', () => {
+        shop.updateQuality();
+        expect(shop.items[0].quality).toEqual(4);
       });
     });
   });
